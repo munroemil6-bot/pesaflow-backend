065e2e2eea2bd487d86a1e5c8b3a52a37d859f17
myles munroe <munroemil6@gmail.com>
2026-08-26 08:58:14 +0300

    Remove duplicate accounts route include

--- pesaflow/urls.py
+++ pesaflow/urls.py
@@ -20,7 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include("accounts.urls")),
     
     # Mason: User Authentication & Accounts
     path('api/accounts/', include('accounts.urls')),
