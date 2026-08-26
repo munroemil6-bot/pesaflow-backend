633dfa4fe74ad233633280b358146d007b9fd5bf
myles munroe <munroemil6@gmail.com>
2026-08-26 09:01:20 +0300

    Align beneficiary admin with model fields

--- beneficiaries/admin.py
+++ beneficiaries/admin.py
@@ -17,70 +17,46 @@ Admin configuration to implement:
 # TODO: Register Beneficiary model with BeneficiaryAdmin
 
 from django.contrib import admin
-from django.utils.html import format_html
 from .models import Beneficiary
 
 @admin.register(Beneficiary)
 class BeneficiaryAdmin(admin.ModelAdmin):
     # List Display
     list_display = (
-        'id', 
-        'user', 
-        'name', 
-        'phone', 
-        'email',
-        'bank_name',
-        'is_active',
-        'is_favorite',
-        'created_at_display'
+        'id',
+        'user',
+        'name',
+        'phone',
+        'created_at',
     )
     
     # Search Fields
     search_fields = (
-        'name', 
-        'phone', 
-        'email',
-        'user__username',
+        'name',
+        'phone',
         'user__email',
-        'bank_name',
-        'account_number'
     )
     
     # Filters
     list_filter = (
-        'is_active',
-        'is_favorite',
-        'account_type',
         'created_at',
         'updated_at'
     )
     
     # Read-only fields
     readonly_fields = (
-        'created_at', 
+        'created_at',
         'updated_at',
-        'created_at_display',
-        'updated_at_display'
     )
     
     
     # Fieldsets for detailed view
     fieldsets = (
         ('User Information', {
-            'fields': ('user', 'name', 'phone', 'email')
-        }),
-        ('Bank Details', {
-            'fields': ('bank_name', 'account_number', 'account_type')
-        }),
-        ('Additional Information', {
-            'fields': ('address', 'notes'),
-            'classes': ('collapse',)
-        }),
-        ('Status', {
-            'fields': ('is_active', 'is_favorite')
+            'fields': ('user', 'name', 'phone')
         }),
         ('Timestamps', {
-            'fields': ('created_at', 'updated_at', 'created_at_display', 'updated_at_display'),
+            'fields': ('created_at', 'updated_at'),
             'classes': ('collapse',)
         }),
     )
