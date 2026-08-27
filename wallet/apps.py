d74ceaa043d5ff3cdb3646795030f7aaf07916c0
myles munroe <munroemil6@gmail.com>
2026-08-27 18:00:45 +0300

    Register wallet Django app configuration

--- wallet/apps.py
+++ wallet/apps.py
@@ -11,3 +11,11 @@ This app handles:
 - Transaction history
 """
 
+from django.apps import AppConfig
+
+
+class WalletConfig(AppConfig):
+	default_auto_field = 'django.db.models.BigAutoField'
+	name = 'wallet'
+	verbose_name = 'Wallets'
+
